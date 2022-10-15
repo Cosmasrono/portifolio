d5f31d241e55bb99f3b5ef2c597bced6705a5057
Cosmasrono <ccosmas001@gmail.com>
2022-10-15 14:39:39 +0300

    changes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,46 +1,33 @@
 <template>
-  <main>
-    <div tabindex="3" class="e-card e-card-horizontal flex items-center justify-center  pl-6 px-3 my-4 mx-60 rounded-md border-cyan-300  text-white dark:bg-slate-900 ">
-    <div class="e-card-stacked">
-      <h1 class=" text-2xl flex items-center justify-center text-white" > The Vue</h1>
-   <div class=" flex flex-row"><img src="../assets/one.jpg" class="  pl-2 md:mt-1 md:w-1/5   md:left top-1 md:ml-20 h-auto md:flex">
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et nam deserunt commodi consequatur numquam, aspernatur soluta cum libero? Repudiandae exercitationem alias vero quisquam minus iusto voluptates similique nisi distinctio officiis.
-      
-      </div>
+  <main class=" mb-10">
+    
+    <div class=" flex items-center bg-slate-900 text-white  mt-2 md:flex-row justify-center left-28 relative md:left-24 top-1 w-3/5 flex-row ">
+      <img src="../assets/react.png" alt="" class=" w-24 md:mr-4">
+      <p> Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.</p>
     </div>
-      </div> 
-      <div tabindex="3" class="e-card e-card-horizontal flex  items-center justify-center pl-6 px-3 my-4 mx-60  rounded-md border-cyan-300 text-white bg-slate-900" >
-    <div class="e-card-stacked">
-      <h1 class=" text-2xl flex items-center justify-center text-white"> Html and Css</h1>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et nam deserunt commodi consequatur numquam, aspernatur soluta cum libero? Repudiandae exercitationem alias vero quisquam minus iusto voluptates similique nisi distinctio officiis.
-      
-      
+    <div class=" flex items-center bg-slate-900 text-white mt-2 md:flex-row justify-center left-28 relative md:left-24 top-1 w-3/5 flex-row ">
+      <img src="../assets/services.png" alt="" class=" w-24 md:mr-4">
+      <p> AWS stands for Amazon Web Services which uses distributed IT infrastructure to provide different IT resources on demand.
+
+Our AWS tutorial includes all the topics such as introduction, history of aws, global infrastructure, features of aws, IAM, Storage services, Database services, etc..</p>
     </div>
-      </div> 
-      <div tabindex="3" class="e-card e-card-horizontal   flex items-center justify-center pl-6 px-3 my-4 mx-60  rounded-md border-cyan-300 text-white  dark:bg-slate-900" >
-    <div class="e-card-stacked">
-      <h1 class=" text-2xl flex items-center justify-center text-white"> Tailwind</h1>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et nam deserunt commodi consequatur numquam, aspernatur soluta cum libero? Repudiandae exercitationem alias vero quisquam minus iusto voluptates similique nisi distinctio officiis.
-      
-      
+    <div class=" flex items-center bg-slate-900 text-white mt-2 md:flex-row justify-center left-28 relative md:left-24 top-1 w-3/5 flex-row ">
+      <img src="../assets/cloud.png" alt="" class=" w-24 md:mr-4">
+      <p> Cloud computing is a virtualization-based technology that allows us to create, configure, and customize applications via an internet connection. The cloud technology includes a development platform, hard disk, software application, and database..</p>
     </div>
-      </div> 
-      <div tabindex="3" class="e-card e-card-horizontal  flex items-center justify-center pl-6 px-3 my-4 mx-60  rounded-md border-cyan-300 text-white dark:bg-slate-900" >
-    <div class="e-card-stacked">
-      <h1 class=" text-2xl flex items-center justify-center text-white"> Bootstrap</h1>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et nam deserunt commodi consequatur numquam, aspernatur soluta cum libero? Repudiandae exercitationem alias vero quisquam minus iusto voluptates similique nisi distinctio officiis.
-      
-      
+    <div class=" flex items-center bg-slate-900 text-white mt-2 md:flex-row justify-center left-28 relative md:left-24 top-1 w-3/5 flex-row ">
+      <img src="../assets/blockchain.png" alt="" class=" w-24 md:mr-4">
+      <p> Blockchain Tutorial provides basic and advanced concepts of blockchain. Blockchain is a constantly growing ledger that keeps a permanent record of all the transactions that have taken place in a secure, chronological, and immutable way. It can be used for the secure transfer of money, property, contracts, etc. without requiring a third-party intermediary such as bank or government. Blockchain is a software protocol, but it could not be run without the Internet (like SMTP is for email).</p>
     </div>
-      </div> 
-      <div tabindex="3" class="e-card e-card-horizontal   flex items-center justify-center pl-6 px-3 my-4 mx-60  rounded-md border-cyan-300 text-white bg-slate-900" >
-    <div class="e-card-stacked">
-      <h1 class=" text-2xl flex items-center justify-center text-white"> Python</h1>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et nam deserunt commodi consequatur numquam, aspernatur soluta cum libero? Repudiandae exercitationem alias vero quisquam minus iusto voluptates similique nisi distinctio officiis.
-      
-      
+    <div class=" flex items-center bg-slate-900 text-white mt-2 md:flex-row justify-center left-28 relative md:left-24 top-1 w-3/5 flex-row ">
+      <img src="../assets/artificial.png" alt="" class=" w-24 md:mr-4">
+      <p> Here, one of the booming technologies of computer science is Artificial Intelligence which is ready to create a new revolution in the world by making intelligent machines.The Artificial Intelligence is now all around us. It is currently working with a variety of subfields, ranging from general to specific, such as self-driving cars, playing chess, proving theorems, playing music, Painting, etc.</p>
     </div>
-      </div> 
+    <div class=" flex items-center bg-slate-900 text-white mt-2 md:flex-row justify-center left-28 relative md:left-24 top-1 w-3/5 flex-row ">
+      <img src="../assets/regex.png" alt="" class=" w-24 md:mr-4">
+      <p> This concept or tool is used in almost all the programming or scripting languages such as PHP, C, C++, Java, Perl, JavaScript, Python, Ruby, and many others. It is also used in word processors such as word which helps users for searching the text in a document, and also used in various IDEs.
+The pattern defined by the regular expression is applied to the given string or a text from left to right..</p>
+    </div>
   </main>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,11 @@
 <template>
+  <main class="">
 <div class=" bg-slate-500 ">
     <h1 class=" flex items-center justify-center"> Hello here is cosmas feel free and relax </h1>
     <div class=" text-black text-xl  md:mt-1 md:left top-1 md:ml-2  md:relative  md:flex ">
       <div class=" relative left-14"> 
    <div class=" flex items-center justify-end pr-6 pl-6 px-3 my-4 mx-60 rounded-md border-slate-700  text-white">
-      <div class=" "><img src="../assets/five.jpg" class="  h-full w-full ">
+      <div class=" "><img src="../assets/lepen.jpg" class="  h-full w-full ">
     </div>
    </div>
       </div>
@@ -27,6 +28,7 @@
   </div>
       </aside>
       <div class=" pt-3 mt-3 md:flex-row ml-3">
+
       <div class=" w-4/5 relative z-10 md:left-40 ">
         <div class=" md:flex-row w-full justify-center items-center md:min-h-screen relative ">
       
@@ -34,24 +36,26 @@
       <div class="  flex justify-center items-center min-h-screen h-full  left-16 relative md:left-28 top-1 z-10 w-4/5 flex-col">
       
         
-      <div class=" bg-slate-900 ">
+      <div class=" bg-slate-900 hover:scale-90 ">
+        <h1>Programming languages
+        </h1>
         <h1 class=" text-white text-2xl">The vue</h1> 
 
         <div class=" flex  flex-col md:flex-row justify-center items-center m-5 ">
       <div class=" transition-all duration-500 transform hover:opacity-90 pt-1 hover:pb-1">
-        <img src="../assets/five.jpg" alt="" class=" w-60 md:mr-80">
+        <img src="../assets/six.jpg" alt="" class=" w-60 md:mr-80">
       </div>
         <p class=" bg-white">Vue.js is a popular JavaScript framework for front-end development developed and constantly updated by Evan You and his team. You can use this open-source framework to develop any single-page application or user interface. The design is very flexible and focuses on declarative rendering and component composition. </p>
       
       </div>
       </div>
-       <div class=" pt-3  bg-slate-900 mt-3">
+       <div class=" pt-3  bg-slate-900 mt-3  hover:scale-90">
         <h1 class=" text-white text-2xl">NodeJs</h1> 
       <div class=" flex  flex-col md:flex-row justify-center items-center m-5 ">  
         
       
       <div class=" transition-all duration-500 transform hover:opacity-90 pt-1 hover:pb-1">
-        <img src="../assets/five.jpg" alt="" class="  w-56 md:mr-80">
+        <img src="../assets/two.jpg" alt="" class="  w-56 md:mr-80">
       </div>
         <p class=" bg-white">Node.js is an open source, cross-platform runtime environment for developing server-side and networking applications. Node.js applications are written in JavaScript, and can be run within the Node.js runtime on OS X, Microsoft Windows, and Linux.
 
@@ -59,7 +63,7 @@ Node.js also provides a rich library of various JavaScript modules which simplif
       
       </div>
       </div>
-      <div class=" pt-3  bg-slate-900 mt-3">
+      <div class=" pt-3  bg-slate-900 mt-3 hover:scale-90">
         <h1 class=" text-white text-2xl">Tailwind</h1> 
       <div class=" flex  flex-col md:flex-row justify-center items-center m-5 "> 
       
@@ -73,12 +77,12 @@ It's fast, flexible, and reliable — with zero-runtime.  </p>
       
       </div>
       </div>
-      <div class=" pt-3  bg-slate-900 mt-3">
+      <div class=" pt-3  bg-slate-900 mt-3 hover:scale-90">
         <h1 class=" text-white text-2xl">Bootstrap</h1> 
       <div class=" flex  flex-col md:flex-row justify-center items-center m-5 "> 
       
       <div class=" transition-all duration-500 transform hover:opacity-90 pt-1 hover:pb-1">
-        <img src="../assets/five.jpg" alt="" class="  w-56 md:mr-80">
+        <img src="../assets/twendi.jpg" alt="" class="  w-56 md:mr-80">
       </div>
       <h1></h1>
         <p class=" bg-white">Bootstrap makes responsive web design a reality. It makes it possible for a web page or app to detect the visitor's screen size and orientation and automatically adapt the display accordingly. The mobile-first approach assumes smartphones, tablets and task-specific mobile apps are employees' primary tools for getting work done. Bootstrap addresses the requirements of those technologies in design and includes UI components, layouts, JavaScript tools and the implementation framework. The software is available precompiled or as source code.</p>
@@ -95,7 +99,13 @@ It's fast, flexible, and reliable — with zero-runtime.  </p>
       
      
 </div>
-
+<span class=" flex flex-row items-center justify-center gap-5  bg-slate-900 mt-4 pt-3 ml-72 mr-52 pb-10">
+                                  <a href="https://www.facebook.com/"><img src="../assets/facebook.png"></a>
+                                  <a href="https://web.whatsapp.com/"><img src="../assets/whatsapp.png"></a>
+                                  <a href="https://www.linkedin.com/feed/"><img src="../assets/linkedin.png"></a>
+                                 
+                           </span>
+</main>
 </template>
 
 <script>
